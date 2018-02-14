7d3b092615e61d15c61e6dea26a20aaa077d36ad
randombotmaker <36473821+randombotmaker@users.noreply.github.com>
2018-02-14 21:22:05 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+
+const PREFIX = "";
+
+var client = new Discord.Client();
+
+const TOKEN = "NDEzMjMwODM0MDYxOTM0NTk3.DWVzfw.bWTpgGVyounGXXHb_01MT2ceeio";
+
+client.on("ready", function(message) {
+    console.log("Logged in as ${client.user.tag}")
+    client.user.setStatus('Online')
+    client.user.setGame('&help')
+});
+
+client.on("message", function(message) {
+    if(message.author.equals(client.user)) return;
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+
+    case "&ping":
+    message.channel.sendMessage("pong!")
+    break;
+            
+    case "hi":
+    message.channel.sendMessage("hey")
+    break;
+            
+    case "&help":
+    message.channel.sendMessage("Help commands coming soon!")
+    break;
+            
+    case "&pong":
+    message.channel.sendMessage("ping?")
+    break;
+    }
+});
+client.login(TOKEN)
